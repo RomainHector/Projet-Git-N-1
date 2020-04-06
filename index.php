dab850cbd9d720eca896f8921165c02163afd535
Grégory LACROIX <gregorylacroix78@gmail.com>
2020-04-06 15:48:48 +0200

    ajout_slider + modif divers greg

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
 
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+    <a class="navbar-brand text-white" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -17,46 +17,83 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link text-white" href="#">Home <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">About</a>
+            <a class="nav-link text-white" href="#">About</a>
         </li>
     </div>
     </nav>
 
-    <div class="container">
+    <div id="carouselExampleCaptions" class="col-md-8 mx-auto carousel slide rounded my-4" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img src="https://picsum.photos/seed/picsum/300/100" class="d-block w-100 rounded" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>First slide label</h5>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </div>
+            </div>
+            <div class="carousel-item">
+            <img src="https://picsum.photos/seed/picsum/300/100" class="d-block w-100 rounded" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Second slide label</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+            </div>
+            <div class="carousel-item">
+            <img src="https://picsum.photos/seed/picsum/300/100" class="d-block w-100 rounded" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Third slide label</h5>
+                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            </div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
 
-        <h1>Projet Git n°1 /!\ il faut pas modifier le titre</h1>
+    <div class="container" style="min-height: 50vh">
 
-        <div class="row border border-dark p-4">
+        <hr><h1 class='text-center'>Projet Git n°1 /!\ il faut pas modifier le titre</h1>
+
+        <div class="row col-md-8 mx-auto border border-dark rounded p-4 my-4">
             <div class="col-md-3 bg-info">C'est la zone 1 </div>
-            <div class="col-md-6 border">Greg addore le confinement</div>
+            <div class="col-md-6 border">Greg addore le confinement et l'apéro</div>
             <div class="col-md-3 bg-danger">Zone 3</div>
         </div>
-        <div class="row bg-secondary m-1 p-2" style="height: 50vh">
-            <form action="" method="POST" class="form">
-            <select class="form-control form-control-lg text-center">
-                <option>Choisis ta boisson 5L</option>
-                <option value="">La bière</option>
-                </select>
-                <select class="form-control text-center">
-                <option>2L</option>
-                </select>
-                <select class="form-control form-control-sm text-center">
+
+        <div class="row bg-secondary col-md-5 mx-auto m-1 p-2 rounded">
+            <form action="" method="POST" class="form mx-auto">
+            <select class="form-control form-control-lg text-center mb-2">
+                <option>Choisis ta boisson Bière</option>
+                <option>5L</option>
+                <option>3L</option>
                 <option>1L</option>
             </select>
-            <button type="submit" class="btn btn-danger"> bouton !</button>
+            <button type="submit" class="btn btn-danger"> Buvez !!</button>
             </form>
         </div>
-      
-        <footer class="bg-dark text-center p-4 text-danger">
-            A toi de jouer !
-        </footer>
+    
     </div>
 
     <footer class="bg-dark text-center p-4 text-danger">
         A toi de jouer !
     </footer>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 </html>
