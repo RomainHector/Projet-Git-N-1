6adde74e53e2265beed1475f68835635098d02a4
Grégory LACROIX <gregorylacroix78@gmail.com>
2020-04-06 15:06:30 +0200

    modif index greg

--- index.php
+++ index.php
@@ -8,20 +8,47 @@
 
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+        </li>
+    </div>
+    </nav>
 
     <div class="container">
-        <h1>Projet Git n°1</h1>
+        <h1>Projet Github Greg (mon titre est beaucoup mieux)</h1>
 
-        <div class="row border border-dark p-4">
-            <div class="col-md-3 bg-info"></div>
-            <div class="col-md-6 border"></div>
-            <div class="col-md-3 bg-danger"></div>
-        </div>
-      
-        <footer class="bg-dark text-center p-4 text-danger">
-            A toi de jouer !
-        </footer>
+        <form class="col-md-6 mx-auto">
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Objet</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlTextarea1">Message</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Envoyer</button>
+        </form>
     
     </div>
+
+    <footer class="bg-dark text-center p-4 text-danger">
+        A toi de jouer !
+    </footer>
 </body>
 </html>
