7354f01b881e4db798f65e68be0217a33d42bb73
Romain HECTOR <romainhector@yahoo.fr>
2020-04-06 14:32:17 +0200

    branche_romain

--- index.php
+++ index.php
@@ -8,6 +8,20 @@
 
 </head>
 <body>
-    <h1>coucou</h1>
+
+    <div class="container">
+        <h1>Projet Git n°1</h1>
+
+        <div class="row border border-dark p-4">
+            <div class="col-md-3 bg-info"></div>
+            <div class="col-md-6 border"></div>
+            <div class="col-md-3 bg-danger"></div>
+        </div>
+      
+        <footer class="bg-dark text-center p-4 text-danger">
+            A toi de jouer !
+        </footer>
+    
+    </div>
 </body>
 </html>
